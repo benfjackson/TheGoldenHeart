d5603e9b59118b6a0ccea54017411a9b787e0798
SirLoddington <benfischerj@gmail.com>
2025-03-24 17:49:01 +1100

    reformat

--- components/Tapper.jsx
+++ components/Tapper.jsx
@@ -1,31 +1,16 @@
-import React, { useState, useEffect, useRef, useMemo } from 'react';
-import { View, Text, Image, StyleSheet, PanResponder } from 'react-native';
+import React, { useState, useRef, useMemo } from 'react';
+import { View, Text, StyleSheet, PanResponder } from 'react-native';
 
-export default function Tapper({
-  skull = true,
-  inverse = false,
-  life,
-  setLife,
-  size = 'guest'
-}) {
+export default function Tapper({ inverse = false, life, setLife }) {
   const countBoxRef = useRef(null);
   const [countBoxTop, setCountBoxTop] = useState();
   const [countBoxBottom, setCountBoxBottom] = useState(0);
-  const skullIcon = require('../icons/skullWhite.png');
-
-  // const sizeMap = {
-  //   guest: 100,
-  //   counter: 60
-  // };
 
   const calcNewCount = (y) => {
     const centreOfBox = (countBoxTop + countBoxBottom) / 2;
-    // console.log('y', y);
-    // console.log('centreOfBox', centreOfBox);
-
-    var toAdd = y < centreOfBox ? -1 : 1;
-    var inverseN = inverse ? 1 : -1;
-    var newCount = life + toAdd * inverseN;
+    const toAdd = y < centreOfBox ? -1 : 1;
+    const inverseN = inverse ? 1 : -1;
+    const newCount = life + toAdd * inverseN;
     if (life === 0 && toAdd * inverseN < 0) {
       return 0;
     } else {
@@ -57,30 +42,15 @@ export default function Tapper({
             setCountBoxBottom(height + pageY);
           });
         }}>
-        {/* TO ROTATE 180, 
-        <Image style={{ width: 200, height: 200, transform: [{ rotate: '180deg' }] }} source={skullIcon} />
-        */}
-
-        {life > 0 || !skull ? (
-          <Text
-            style={{
-              fontSize: 80,
-              color: '#ffffffa0',
-              transform: [{ rotate: inverse ? '180deg' : '0deg' }],
-              fontFamily: 'Immortal'
-            }}>
-            {life}
-          </Text>
-        ) : (
-          <Image
-            style={{
-              width: 200,
-              height: 200,
-              transform: [{ rotate: '180deg' }]
-            }}
-            source={skullIcon}
-          />
-        )}
+        <Text
+          style={{
+            fontSize: 80,
+            color: '#ffffffa0',
+            transform: [{ rotate: inverse ? '180deg' : '0deg' }],
+            fontFamily: 'Immortal'
+          }}>
+          {life}
+        </Text>
       </View>
       <Text style={{ opacity: 0 }}> HI LIVI!!!</Text>
     </View>
